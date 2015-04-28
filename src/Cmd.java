c97bebf399e1681797f90275a02c37dc5db939a7
zhangxiaopeng <zhangxiaopeng@D-F004860.1verge.com>
2015-04-28 19:02:55 +0800

    Signed-off-by: zhangxiaopeng <zhangxiaopeng@D-F004860.1verge.com>

---
+++ src/Cmd.java
@@ -0,0 +1,154 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Cmd{
+
+    /*
+     * 返回结果为一个长度为2的数组 List[0]是常规回显 List[1]是错误回显
+     * programAndArguments:在windows上执行为cmd linux上为 /bin/sh
+     * ev:环境变量键值对
+     * dir:执行目录
+     * String:命令数组
+     * timeout:超时时间 单位为分钟
+     */
+    public static List[] execCmdsInProcess(String[] programAndArguments,Map<String,String> ev, String dir, String[] cmds,int timeout){
+        Process proc = null;
+        OutputStream os = null;
+        InputStream is = null;
+        InputStream eis = null;
+        List<String> echoList = new ArrayList<String>();
+        List<String> errEchoList = new ArrayList<String>();
+        
+        try {
+            ///*
+            ProcessBuilder pb = new ProcessBuilder(programAndArguments);
+            //ProcessBuilder pb = new ProcessBuilder("cmd");
+            //ProcessBuilder pb = new ProcessBuilder("/bin/sh");
+            Map<String,String> env = pb.environment();
+            if(ev != null){
+                env.putAll(ev);
+            }
+            if(dir != null){
+                pb.directory(new File(dir));
+            }
+            System.out.println("Cmd process start.");
+            proc = pb.start();
+            
+            Timer timer = new Timer();
+            timer.schedule(new ProcessTimeoutProctor(proc,errEchoList), 1000*60*timeout);
+            
+            //proc = Runtime.getRuntime().exec("/bin/sh");
+            //proc = Runtime.getRuntime().exec("cmd");
+            os = proc.getOutputStream();
+            is = proc.getInputStream();
+            eis = proc.getErrorStream();
+            
+            EchoProcessThread echoThread = new EchoProcessThread(is,echoList,"Echo");
+            echoThread.start();
+            EchoProcessThread errEchoThread = new EchoProcessThread(eis,errEchoList,"Err Echo");
+            errEchoThread.start();
+
+            for(String cmd : cmds){
+                System.out.println("Cmd input:"+cmd);
+                os.write((cmd + "\n").getBytes());
+                os.flush();
+            }
+            
+            proc.waitFor();
+            timer.cancel();//退出进程保护任务
+            proc.destroy();
+            System.out.println("Cmd process end.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error occured in cmd executing:" + e.getMessage());
+            echoList.add("Error occured in cmd executing:" + e.getMessage());
+        } finally{
+            try{
+                if(os != null){
+                    os.close();
+                }
+            }catch(Exception e){}
+            try{
+                if(is != null){
+                    is.close();
+                }
+            }catch(Exception e){}
+            try{
+                if(eis != null){
+                    eis.close();
+                }
+            }catch(Exception e){}
+            try{
+                if(proc != null){
+                    proc.destroy();
+                }
+            }catch(Exception e){}
+        }
+        
+        List[] result = new List[]{echoList, errEchoList};
+        
+        return result;
+    }
+    
+}
+
+class ProcessTimeoutProctor extends TimerTask{
+    private Process proc;
+    private List<String> errEchoList;
+    ProcessTimeoutProctor(Process proc,List<String> errEcho){
+        this.proc = proc;
+        this.errEchoList = errEcho;
+    }
+    public void run(){
+        errEchoList.add("Process is terminated automaticlly for timeout");
+        proc.destroy();
+    }
+}
+
+class EchoProcessThread extends Thread{
+    private InputStream is = null;
+    private List<String> echoList = null;
+    private String info = null;
+    
+    public EchoProcessThread(InputStream is, List<String> echoList,String info){
+        this.is = is;
+        this.echoList = echoList;
+        this.info = info;
+    }
+    
+    public void run(){
+        System.out.println("Sub thread("+this.info+")begin.");
+        InputStreamReader isr = new InputStreamReader(this.is);
+        BufferedReader br = new BufferedReader(isr);
+        String oneLine = null;
+        try{
+            while((oneLine = br.readLine()) != null){
+                System.out.println(this.info+" info-->"+oneLine);
+                this.echoList.add(oneLine);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Error occured while reading("+this.info+")->"+e.getMessage());
+            this.echoList.add("Error occured while reading("+this.info+")->"+e.getMessage());
+        }finally{
+            try{
+                br.close();
+            }catch(Exception e){}
+            try{
+                isr.close();
+            }catch(Exception e){}
+        }
+        System.out.println("Sub thread("+this.info+")end.");
+    }
+}
+
+
---
+++ src/Role.java
@@ -0,0 +1,268 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.LdapContext;
+
+
+/*
+ * 前提:堡垒的限制逻辑保障用户是否能维护和登录该设备账号
+ * accountLimited的语义定义为:列举改用户对该设备可维护的账号(授权登录的账号才可以维护)
+ * 判定某个人对某个设备是否有限制角色范围内的授权账号，无非分下面四种情况
+ * 1,没有限制角色 => 返回1
+ * 2,有限制角色但是目标设备不在限制角色范围内 => 返回2
+ * 3,有限制角色且目标设备也在限制角色范围内但是无相应授权账号 => 返回3
+ * 4,有限制角色且目标设备也在限制角色范围内而且还有相应授权账号 => 返回授权账号(逗号分隔)
+ */
+
+public class Role {
+    
+    public static void main(String[] args){
+        System.out.println(accountLimited("zhangke","Asset_002A5D869 "));
+//        if(args.length == 3){
+//            System.out.println(accountLimited(args[0],args[1],Boolean.parseBoolean(args[2])));
+//        }
+//        if(args.length == 2){
+//            System.out.println(accountLimited(args[0],args[1]));
+//        }
+    }
+    
+    public static String accountLimited(String personCn, String assetCn){
+    	if(personCn.startsWith("cn=")){
+    		personCn = personCn.substring(3);
+    	}
+    	if(assetCn.startsWith("cn=")){
+    		assetCn = assetCn.substring(3);
+    	}
+        return accountLimited(personCn, assetCn, false);
+    }
+    
+    public static String accountLimited(String personCn, String assetCn, boolean fortEnv){
+        LDAPConnection conn = LDAPEnv.getLDAPConnection(fortEnv);
+        try{
+            List<String> pseronCnInNameSpaceList = new ArrayList<String>();
+        	List<String> roleList = searchRoleLimited(conn, personCn, pseronCnInNameSpaceList);
+        	
+        	String pseronCnInNameSpace = pseronCnInNameSpaceList.get(0);
+            if(pseronCnInNameSpace == null){
+                return "FAIL:1";
+            }
+            
+        	if(roleList == null || roleList.size() < 1){
+        	    return "SUCCESS:1";
+        	}
+        	
+        	String assetCnInNamespace = getAssetCnInNamespace(conn, assetCn);
+        	if(assetCnInNamespace == null){
+        	    return "FAIL:2";
+        	}
+        	
+        	boolean underRoleControl = false;
+        	for(String roleCnInNamespace : roleList){
+        	    System.out.println(roleCnInNamespace);
+        	    underRoleControl = underRoleControl(roleCnInNamespace, assetCnInNamespace);
+        	    if(underRoleControl)break;
+        	}
+        	
+        	if(!underRoleControl){
+        	    return "SUCCESS:2";
+        	}
+        	
+        	List<String> authorizationList = getAuthorization(conn, pseronCnInNameSpace);
+        	
+        	
+        }catch(Exception e){
+            e.printStackTrace();
+            return "FAIL:X";
+        }finally{
+            conn.close();
+        }
+        return "root,support";
+    }
+    
+    private static List<String> searchRoleLimited(LDAPConnection conn, String personCn, List<String> pseronCnInNameSpaceList){
+    	List<String> list = new ArrayList<String>();
+    	LdapContext ctx = conn.getLdapContext();
+        //System.out.println("ctx=" + ctx);
+    	SearchControls sc = new SearchControls();
+    	sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+    	
+    	String searchFilter = "(&(objectclass=simp-person)(cn="+ getCnValue(personCn) +"))";
+    	String searchBase = "ou=People,dc=simp,dc=com";
+    	String rs[] = {
+    	        "cn",
+    	        "name",
+    	        "simp-roles"
+    	};
+    	sc.setReturningAttributes(rs);
+        try {
+        	
+            NamingEnumeration<SearchResult> anser = ctx.search(searchBase, searchFilter, sc);
+            while(anser.hasMoreElements()){
+                SearchResult sr = anser.next();
+                pseronCnInNameSpaceList.add(sr.getNameInNamespace());
+                Attributes attrs = sr.getAttributes();
+                Attribute roleAttribute = attrs.get("simp-roles");
+                if(roleAttribute != null){
+                    int size = roleAttribute.size();
+                    for(int index = 0; index < size; index++){
+                        Object roleObj = roleAttribute.get(index);
+                        if(roleObj != null){
+                            String roleCnInNamespace = roleObj.toString();
+                            //System.out.println(roleObj.toString());
+                            //list.add(nameInNamespace);
+                            String roleName = getRoleName(conn, roleCnInNamespace);
+                            if(roleName != null && roleName.endsWith("@LIMITED")){
+                                list.add(roleCnInNamespace);
+                            }
+                        }
+                    }
+                }
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            
+        }
+        return list;
+    }
+    
+    private static String getRoleName(LDAPConnection conn, String roleCn){
+        
+        LdapContext ctx = conn.getLdapContext();
+        //System.out.println("ctx=" + ctx);
+        SearchControls sc = new SearchControls();
+        sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        
+        String searchFilter = "(&(objectclass=simp-role)(cn="+ getCnValue(roleCn) +"))";
+        String searchBase = "ou=Groups,dc=simp,dc=com";
+        String rs[] = {
+                "cn",
+                "name"
+        };
+        sc.setReturningAttributes(rs);
+        try {
+            NamingEnumeration<SearchResult> anser = ctx.search(searchBase, searchFilter, sc);
+            if(anser.hasMoreElements()){
+                SearchResult sr = anser.next();
+                Attributes attrs = sr.getAttributes();
+                //System.out.println("getNameInNamespace()="+sr.getNameInNamespace());//like : cn=Role_132645461724583,cn=Group_131607006073081,ou=Groups, dc=simp,dc=com
+                Attribute roleAttribute = attrs.get("name");
+                if(roleAttribute != null){
+                    Object roleObj = roleAttribute.get();
+                    if(roleObj != null) {
+                        return roleObj.toString();
+                    }
+                }
+            }
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }finally{
+            
+        }
+        return null;
+    }
+    
+    private static String getAssetCnInNamespace(LDAPConnection conn, String assetCn){
+        assetCn = getCnValue(assetCn);
+        
+        LdapContext ctx = conn.getLdapContext();
+        SearchControls sc = new SearchControls();
+        sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        
+        String searchFilter = "(&(objectclass=simp-asset)(cn="+ getCnValue(assetCn) +"))";
+        String searchBase = "ou=Groups,dc=simp,dc=com";
+        String rs[] = {
+                "cn",
+                "name"
+        };
+        sc.setReturningAttributes(rs);
+        try {
+            NamingEnumeration<SearchResult> anser = ctx.search(searchBase, searchFilter, sc);
+            if(anser.hasMoreElements()){
+                SearchResult sr = anser.next();
+                return sr.getNameInNamespace();
+            }
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }finally{
+            
+        }
+        return null; 
+    }
+    
+    private static boolean underRoleControl(String roleCnInNamespace, String assetCnInNamespace) throws Exception{
+        
+        if(roleCnInNamespace == null)throw new Exception("illegal roleCnInNamespace:" + roleCnInNamespace);
+        if(assetCnInNamespace == null)throw new Exception("illegal assetCnInNamespace:" + assetCnInNamespace);
+        
+        roleCnInNamespace = roleCnInNamespace.replaceAll(" ", "");
+        assetCnInNamespace = assetCnInNamespace.replaceAll(" ", "");
+        
+        String scope = null;
+        if(roleCnInNamespace.indexOf(",") > -1){
+            scope = roleCnInNamespace.substring(roleCnInNamespace.indexOf(",") + 1);
+        }else{
+            throw new Exception("illegal roleCnInNamespace:" + roleCnInNamespace);
+        }
+        
+        if(assetCnInNamespace.endsWith(scope))return true;
+        
+        return false;
+    }
+    
+    private static List<String> getAuthorization(LDAPConnection conn, String personCnInNamespace){
+        List<String> list = new ArrayList<String>();
+        LdapContext ctx = conn.getLdapContext();
+        //System.out.println("ctx=" + ctx);
+        SearchControls sc = new SearchControls();
+        sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        
+        String searchFilter = "(objectclass=simp-authorization)";
+        String searchBase = personCnInNamespace;
+        String rs[] = {
+                "cn",
+                "name",
+                "simp-authorize-account-rdn"
+        };
+        sc.setReturningAttributes(rs);
+        try {
+            NamingEnumeration<SearchResult> anser = ctx.search(searchBase, searchFilter, sc);
+            while(anser.hasMoreElements()){
+                SearchResult sr = anser.next();
+                Attributes attrs = sr.getAttributes();
+                Attribute attribute = attrs.get("simp-authorize-account-rdn");
+                if(attribute != null){
+                    Object obj = attribute.get();
+                    if(obj != null){
+                        String cnInNamespace = obj.toString();
+                        System.out.println(cnInNamespace);
+                        list.add(cnInNamespace);
+                    }
+                }
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            
+        }
+        return list;
+    }
+    
+    private static String getCnValue(String cn){
+        if(cn.indexOf(",") > -1){
+            cn = cn.substring(0, cn.indexOf(","));
+        }
+        if(cn.indexOf("=") > -1){
+            cn = cn.substring(cn.indexOf("=") + 1);
+        }
+        return cn;
+    }
+}
